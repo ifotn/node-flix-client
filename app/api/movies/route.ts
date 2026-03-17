6c36b64d5b9bb00a0a2188927997d391b8d6ce48
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-03-17 15:51:52 -0400

    l10/2 - post movie done

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,9 +1,30 @@
 // route.ts: standard Next.js file name for file used to call external api GET (all) & POST
 
-// GET: /api/games => fetch all games from server api
+// GET: /api/games => fetch all movies from server api
 export async function GET() {
     // use the js fetch lib to call our server api & get a response
     // this connects React client app to Express server app
     const res: Response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/v1/movies`);
     return Response.json(await res.json());
 }
+
+// POST: /api/games => submit new movie to db
+export async function POST(req: Request) {
+    // parse request body
+    const body = await req.json();
+
+    // call server api
+    const res: Response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/v1/movies`, {
+        'method': 'POST',
+        'headers': { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+    });
+
+    // api post failure
+    if (!res.ok) {
+        const errorText: string = await res.text();
+        return new Response( errorText, { status: res.status });
+    }
+    // success
+    return Response.json({ success: true });
+};
--- app/movies/create/page.tsx
+++ app/movies/create/page.tsx
@@ -1,9 +1,17 @@
 'use client';
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function CreateMovie() {
+    // router for redirect
+    const router = useRouter();
+
     // state vars for form inputs
     const [title, setTitle] = useState<string>('');
+    const [year, setYear] = useState<string>('');
+    const [genre, setGenre] = useState<string>('');
+    const [rating, setRating] = useState<string>('');
+    const [duration, setDuration] = useState<string>('');
 
     // state var key/val dictionary of validation errors in form
     const [errors, setErrors] = useState<Record<string, string>>({});
@@ -13,41 +21,72 @@ export default function CreateMovie() {
 
         // check title
         if (!title.trim()) newErrors.title = 'Title is required';
+        if (!year.trim()) newErrors.year = 'Year is required';
+        if (!rating.trim()) newErrors.rating = 'Rating is required';
+
+        if (parseInt(rating) < 0 || parseInt(rating) > 10) newErrors.rating = 'Rating must be from 0-10';
 
         setErrors(newErrors);
 
         // are there any errors in our dictionary?
         if (Object.keys(newErrors).length === 0) {
-            alert('valid');
             return true;
         }
 
-        alert('invalid');
         return false;
     };
+
+    // form submission handler
+    const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+        // disable default form behaviour (i.e. submit to server)
+        e.preventDefault();
+
+        if (!validate()) {
+            return;  // stop and don't submit, validation errors already displayed
+        }
+
+        // submit form data to api 
+        const res: Response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/movies`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                title, year, genre, rating, duration
+            })
+        });
+
+        if (!res.ok) alert('Failed to save movie');
+        else {
+            // refresh movie list
+            router.push('/movies');
+        }      
+    }
 
     return (
         <main>
             <h1>Movie Details</h1>
-            <form onSubmit={validate}>
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <label htmlFor="">Title: *</label>
                     <input name="title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    {errors.title && <span className="error">{errors.title}</span>}
                 </fieldset>
                 <fieldset>
                     <label htmlFor="">Year: *</label>
-                    <input type="text" />
+                    <input name="year" id="year" value={year} onChange={(e) => setYear(e.target.value)} type="number" />
+                    {errors.year && <span className="error">{errors.year}</span>}
                 </fieldset>
                 <fieldset>
                     <label htmlFor="">Genre: </label>
-                    <input type="text" />
+                     <input name="genre" id="genre" value={genre} onChange={(e) => setGenre(e.target.value)} />
                 </fieldset>
                 <fieldset>
                     <label htmlFor="">Rating /10:</label>
-                    <input type="text" /></fieldset>
+                    <input name="rating" id="rating" value={rating} onChange={(e) => setRating(e.target.value)} type="number" />
+                    {errors.rating && <span className="error">{errors.rating}</span>}
+                 </fieldset>              
                 <fieldset>
                     <label htmlFor="">Duration (mins)</label>
-                    <input type="text" />
+                     <input name="duration" id="duration" value={duration} onChange={(e) => setDuration(e.target.value)} type="number" />
                 </fieldset>
                 <button>Save</button>
             </form>
